1fe9c5ce5a3a388c3783028ed24c29f6f9d3b525
Elise <elise.chaldjian@gmail.com>
2023-12-14 23:35:16 +0100

    Réalisation de test unitaire

--- src/Calculator.php
+++ src/Calculator.php
@@ -8,4 +8,17 @@ class Calculator
     {
         return $firstValue + $secondValue;
     }
+
+    public function multiplication(int $firstValue, int $secondValue): int
+    {
+        return $firstValue * $secondValue;
+    }
+
+    public function isEven(int $firstValue): bool
+    {
+        if($firstValue % 2 === 0) {
+            return true;
+        } 
+        return false;
+    }
 }
--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -22,4 +22,37 @@ class CalculatorTest extends TestCase
             "La méthode sum() ne fonctionne plus :-("
         );
     }
+
+    public function testMultiplication(): void
+    {
+        //Arrange
+        $firstValue = 5;
+        $secondValue = 3;
+        $expectedResult = 15;
+
+        //Act
+        $calculator = new Calculator();
+        $multiplication = $calculator->multiplication($firstValue, $secondValue);
+
+        //Assert
+        $this->assertSame($expectedResult, $multiplication,
+            "La méthode multiplication a échoué"
+        );
+    }
+
+    public function testIsEven(): void
+    {
+        //Arrange
+        $firstValue = 2;
+        $expectedResult = true;
+
+        //Act
+        $calculator = new Calculator();
+        $modulo = $calculator->isEven($firstValue);
+
+        //Assert
+        $this->assertTrue($expectedResult === $modulo,
+            "Le chiffre n'est pas pair"
+        );
+    }
 }
